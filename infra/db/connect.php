c4eec07601af7b7023dc87e0eba7de27f3efee34
franciscohgoulart <francisco_h_machado@estudante.sesisenai.org.br>
2026-06-02 09:47:43 -0300

    ADD- DB
    -explicações da conexão do banci de dados
    -verifica se a conexão foi bem-sucedida

--- infra/db/connect.php
+++ infra/db/connect.php
@@ -1,12 +1,14 @@
 <?php
-
+// Arquivo responsável por estabelecer a conexão com o banco de dados MySQL usando as credenciais fornecidas e verificar se a conexão foi bem-sucedida ou se ocorreu um erro.
     $host = "localhost";
     $user = "root";
     $pass = "root";
     $db = "sistema_simples";
+    // As variáveis acima armazenam as informações necessárias para estabelecer a conexão com o banco de dados.
 
     $conn = new mysqli($host,$user,$pass,$db);
 
+// Verificar se a conexão foi bem-sucedida ou se ocorreu um erro e exibir a mensagem correspondente no console do navegador.
     if ($conn->connect_error){
         echo "<script> console.log('erro na conexão com o banco') </script>";
     }else{
